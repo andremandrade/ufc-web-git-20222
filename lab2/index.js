b9ce0f00b84145afa31c50ec63f7a9cd43e307ac
Andre Meireles <andre@crateus.ufc.br>
2022-09-14 18:37:56 -0300

    Feature mirror working nice

--- lab2/index.js
+++ lab2/index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.post('/mirror', function (req, res, next) {
+  console.log(req.body)
+  res.send(req.body)
+})
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
