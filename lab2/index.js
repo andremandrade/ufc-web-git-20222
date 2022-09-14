db1140d244373a1c9410f26cbcb88ecf66094144
Andre Meireles <andre@crateus.ufc.br>
2022-09-14 18:51:51 -0300

    Feature querytojson working nice

--- lab2/index.js
+++ lab2/index.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/querytojson', (req, res) => {
+  console.log(req.query);
+  res.send(req.query);
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
