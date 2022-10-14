60741f715b894a7e0561a359152ec3667fe77dc1
Andre Meireles <andre@crateus.ufc.br>
2022-10-14 19:29:31 -0300

    Express Basic Auth

--- express-basic-auth/index.js
+++ express-basic-auth/index.js
@@ -14,9 +14,7 @@ const mongoRepository = require('./repository/mongo-repository')
 
 app.use((req, res, next) => {
   console.log('meu middleware')
-  // console.log(req.headers);
   next();
-  // res.send('meu middleware bloqueou')
 })
 
 app.use(basicAuth({
@@ -26,24 +24,59 @@ app.use(basicAuth({
 }));
 
 function userAuthorizer(username, password, callback){
+  console.log('=== UserAuthorizer Middleware');
+  console.log('Username: ' + username)
+  console.log('Password: ' + password)
+
   mongoRepository.getUsers(username, password).then(users => {
-    console.log('userAuthorizer');
     console.log(users);
     callback(null, users.length == 0 ? false : true);
   });
 }
 
-app.get('/home',(req,res) => {
+app.use((req, res, next) => {
+  console.log('=== User Session Middleware');
   basic = Buffer.from(req.headers.authorization.split(' ')[1], 'base64').toString().split(':');
   username = basic[0];
   password = basic[1];
   mongoRepository.getUsers(username, password).then(users => {
-    console.log('/home');
     loggedUser = users[0];
     console.log(loggedUser);
-    res.render('home', {user: loggedUser})
+    req.user = loggedUser;
+    next()
   });
 })
+
+app.get('/home',(req,res) => {
+    console.log('=== GET -/home');
+    console.log(req.user);
+    res.render('home', {user: loggedUser})
+})
+
+app.get('/prod/new', (req, res) => {
+    res.render('prod/new', { loggedUser: req.user})
+})
+
+app.post('/prod/new', (req, res) => {
+  console.log('POST - /prod/new')
+  let newProd = req.body;
+  newProd.createdBy = req.user;
+  newProd.price = parseFloat(newProd.price)
+  console.log(newProd)
+  mongoRepository.saveProd(req.body).then((insertedProd) => {
+    console.log('Inserted Product')
+    console.log(insertedProd)
+    res.redirect('/prod/list')
+  })
+})
+
+app.get('/prod/list', (req, res) => {
+  console.log('GET - /prod/list')
+  mongoRepository.getProdsByUser(req.user).then((foundProds) => {
+    res.render('prod/list', { loggedUser: req.user, products: foundProds})
+  })
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- express-basic-auth/repository/mongo-repository.js
+++ express-basic-auth/repository/mongo-repository.js
@@ -8,12 +8,15 @@ const client = new MongoClient(url);
 const dbName = 'ufcweb2022';
 
 var user_collection;
+var product_collection;
+
 async function main() {
   // Use connect method to connect to the server
   await client.connect();
   console.log('Connected successfully to Mongo server');
   const db = client.db(dbName);
   user_collection = db.collection('user');
+  product_collection = db.collection('product');
   // the following code examples can be pasted here...
    
   return 'done.';
@@ -26,8 +29,26 @@ main()
 
 async function getUsers(username, password) {
     const findResult = await user_collection.find({username: username, password: password}).toArray();
-    console.log('Found documents =>', findResult);
+    console.log('Repository - getUsers - Found documents =>', findResult);
     return findResult;
 }
 
+async function saveProd(product){
+  const result = await product_collection.insertOne(product)
+  console.log('Repository - saveProd - Inserted prod')
+  console.log(result)
+  return result;
+}
+
+async function getProdsByUser(user) {
+  console.log('getProdsByUser - Username param:', user.username)
+  
+  const query = { "createdBy.username": user.username };
+  const findResult = await product_collection.find(query).toArray();
+  console.log('Repository - getProdsByUser - Found documents =>', findResult);
+  return findResult;
+}
+
 exports.getUsers = getUsers;
+exports.saveProd = saveProd;
+exports.getProdsByUser = getProdsByUser;
